53a5224f58ce8c1cce4be87f80f05d18a0fe53cb
dishak11 <dishakothari1198@gmail.com>
2017-06-18 23:31:19 +0530

    uploading the file

---
+++ stopwatch.py
@@ -0,0 +1,32 @@
+import os
+import time
+h=int(raw_input("hrs=>"))
+m=int(raw_input("min=>"))
+s=int(raw_input("sec=>"))
+t=h*3600+m*60+s
+a=1
+check=0
+while a!=t+1:
+	start=int(time.clock()-check)			
+	check=start
+	while start!=1:
+		start=int(time.clock()-check)
+	os.system("clear")
+	print "%s:%s:%s" %(a/3600,(a%3600)/60,a-a/3600-(a%3600)/60)
+	check=time.clock()
+	a+=1
+os.system("say 'beep beep beep beep beep. Time is up.'")
+print time.clock()
+
+
+
+
+'''
+a=int(time.clock())
+while a!=t:
+	if (time.clock()-int(time.clock()))==0:
+		os.system ("clear")
+		print "%s:%s:%s" %(a/3600,(a%3600)/60,a-a/3600-(a%3600)/60)
+	a=int(time.clock())
+os.system("say 'beep beep beep beep beep. Time is up.'")
+'''
