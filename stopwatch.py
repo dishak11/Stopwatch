3742112ae2f611fc17bdd4ae74236d7b05715654
dishak11 <dishakothari1198@gmail.com>
2017-06-19 15:51:39 +0530

    reduced time complexity

--- stopwatch.py
+++ stopwatch.py
@@ -4,15 +4,25 @@ h=int(raw_input("hrs=>"))
 m=int(raw_input("min=>"))
 s=int(raw_input("sec=>"))
 t=h*3600+m*60+s
-a=1
+h=0
+m=0
+s=0
+a=0
 check=0
-while a!=t+1:
+while a!=t:
 	start=int(time.clock()-check)			
 	check=int(time.clock())
 	while start!=1:
 		start=int(time.clock()-check)
+	s+=1
+	if s>=60:
+		s=s-60
+		m+=1
+	if m>=60:
+		m=m-60
+		h+=1
 	os.system("clear")
-	print "%s:%s:%s" %(a/3600,(a%3600)/60,a-a/3600-(a%3600)/60)
+	print "%s:%s:%s" %(h,m,s)
 	check=time.clock()
 	a+=1
 os.system("say 'beep beep beep beep beep. Time is up.'")
